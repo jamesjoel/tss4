ff80c9ff635ce69650d017db023dd2e439d3a9c1
meghasahu9 <meghasahu954@gmail.com>
2019-02-27 19:32:07 +0530

    update by anil server,localhost

--- anil/node1js/index.js
+++ anil/node1js/index.js
@@ -1,6 +1,23 @@
 var express = require("express");
+
 var index = express();
 
-index.listen(3004,function(){
-	console.log("Server Running")
+index.listen(3000,function(){
+    console.log("Server Running");
 });
+
+ index.get("/about",function(req,res){
+ 	console.log("About page is running");
+ 	res.sendFile(__dirname + "/about.html");
+ });
+ index.get("/contact",function(rq,rs){
+ 	console.log("This is contact page");
+ 	rs.sendFile(__dirname + "/contact.html")
+ });
+
+ index.get("/",function(req,res){
+ 	console.log("This is home page");
+ 	res.sendFile(__dirname + "/index.js");
+ });
+
+ index.get("")
