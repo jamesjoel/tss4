bf8116626219fdfd539a910e37ddce602ae5cbdf
tahajaffri123 <tahajaffri87@gmail.com>
2019-03-16 12:54:41 +0530

    update by taha

--- taha/project/app.js
+++ taha/project/app.js
@@ -8,16 +8,13 @@ app.use(express.static(__dirname+"/public/"));
 app.use(bodyParser());
 
 app.get("/",function (req, res) {
-	var pagedata= { title: "Home page"};
-	res.render("home", pagedata);
+	var pagedata= { title: "Home page", pagename: "home"};
+	res.render("layout", pagedata);
 });
-app.get("/premium Graphic card",function (req, res) {
-	var pagedata= { title: "premium Graphic card"};
-	res.render("slider", pagedata);
-});
-app.get("*",function (req, res) {
 	
-	res.render("/nopage")
+app.get("*",function (req, res) {
+	var pagedata={title: "",pagename: "nopage"};
+	res.render("layout", pagedata)
 
 });
 
