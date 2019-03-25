2ea2fc1af43a85d41f19d2641f30381ada797aa7
Anil Singh Chawda <anilsinghchawda@gmail.com>
2019-03-25 07:20:44 +0530

    update by anil

--- anil/mvc1/app.js
+++ anil/mvc1/app.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
-var flash = require("express-flash");
 var session = require("express-session");
+var flash = require("express-flash");
 
 
 
@@ -12,12 +12,12 @@ var routes = require("./config/routes");
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname +"/public/"));
-app.use(routes);
 app.use(bodyParser());
-app.use(flash());
 app.use(cookieParser());
 app.use(session({ secret : "anil"}));
+app.use(flash());
 
+app.use(routes);
 
 app.use(function(req,res,next){
 	res.locals.logo = "Zexome";
--- anil/mvc1/config/admin.js
+++ anil/mvc1/config/admin.js
@@ -2,7 +2,7 @@ var express = require ("express");
 var routes = express.Router();
 
 routes.use("/admin",require("../../controllers/admin/addproduct"));
-routes.use("/");
+// routes.use("/");
 
 
 module.exports=routes;
