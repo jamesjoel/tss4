dbd995b9c363df97c70bee265d129a96ee95bfa0
Anil Singh Chawda <anilsinghchawda@gmail.com>
2019-04-04 07:07:28 +0530

    ...

--- anil/mvc1/config/adminRoutes.js
+++ anil/mvc1/config/adminRoutes.js
@@ -6,6 +6,7 @@ routes.use("/", require("../controllers/admin/login"));
 routes.use("/addcategory", backdoor, require("../controllers/admin/addcategory"))
 routes.use("/viewcategory", backdoor, require("../controllers/admin/viewcategory"));
 routes.use("/viewcategory/editcategory", backdoor, require("../controllers/admin/editcategory"));
+routes.use("/addproduct", backdoor, require("../controllers/admin/addproduct"));
 
 routes.get("/logout", function(req, res){
 	req.session.destroy();
