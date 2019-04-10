cc4c34dba3d1101daafe0c0baf11c789d164232b
jamesjoel <james.joelj@gmail.com>
2019-04-10 18:54:49 +0530

    angular day1

---
+++ angular1/config/routes.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var routes = express.Router();
+
+routes.use("/", require("../controllers/home"));
+
+
+module.exports=routes;
---
+++ angular1/public/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope){
+	
+	$scope.a = "rohit";
+	$scope.b="male";
+
+});
